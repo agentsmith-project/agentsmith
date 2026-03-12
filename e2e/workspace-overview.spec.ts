1b695c0b53eecc9e26c668175d337b0c1ae6c8a2
Zhijian Li <lzjever@hotmail.com>
2026-03-12 09:35:39 -0700

    test: cover workspace home to projects flow

--- e2e/workspace-overview.spec.ts
+++ e2e/workspace-overview.spec.ts
@@ -29,3 +29,19 @@ test.describe('Workspace Overview', () => {
     await expect(authedPage.getByTestId('workspace-overview__empty-filtered')).toBeVisible();
   });
 });
+
+test.describe('Workspace Business Entry', () => {
+  test('workspace home opens projects and projects can return to workspace home', async ({ authedPage }) => {
+    await goTo(authedPage, '/en-US/workspaces/ws_default');
+
+    await expect(authedPage.getByTestId('workspace-home__page')).toBeVisible();
+    await authedPage.getByTestId('workspace-home__open-projects').click();
+
+    await authedPage.waitForURL(/\/en-US\/workspaces\/ws_default\/projects$/, { timeout: 10_000 });
+    await expect(authedPage.getByTestId('projects__back-to-workspace')).toBeVisible();
+
+    await authedPage.getByTestId('projects__back-to-workspace').click();
+    await authedPage.waitForURL(/\/en-US\/workspaces\/ws_default$/, { timeout: 10_000 });
+    await expect(authedPage.getByTestId('workspace-home__page')).toBeVisible();
+  });
+});
